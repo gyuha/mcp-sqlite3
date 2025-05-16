93a97eb580236b895f9d5c56e9c33bae40177ccb
gyuha <nicegyuha@gmail.com>
2025-05-16 22:47:35 +0900

    feat: Update Next.js configuration to include image domains and sqlite3 externals

--- app/next.config.ts
+++ app/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['placehold.co'],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('sqlite3')
+    }
+    return config
+  },
 };
 
 export default nextConfig;
