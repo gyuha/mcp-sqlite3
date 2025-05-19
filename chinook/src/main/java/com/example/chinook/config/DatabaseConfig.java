1c3eedab638b203962c096eed5f12bd697797d3c
gyuha <nicegyuha@gmail.com>
2025-05-19 20:35:41 +0900

    update

--- chinook/src/main/java/com/example/chinook/config/DatabaseConfig.java
+++ chinook/src/main/java/com/example/chinook/config/DatabaseConfig.java
@@ -1,6 +1,5 @@
 package com.example.chinook.config;
 
-import org.hibernate.dialect.SQLiteDialect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
